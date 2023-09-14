f0e8bb6caee02595612b23a0dfa366bf758a1b16
Akash Singh <akashkumarsingh11032001@gmail.com>
2023-09-14 20:50:20 +0530

    progress bar

--- output.js
+++ output.js
@@ -1,20 +1,35 @@
 // measure time for executing a single function.
-const sum = (a,b) => {
-    console.log(`Sum of ${a} and ${b} is: ${a+b}`);
-}
 
-const multiply = (a,b) => {
-    console.log(`Multiply of ${a} and ${b} is: ${a*b}`);
-}
+// const sum = (a,b) => {
+//     console.log(`Sum of ${a} and ${b} is: ${a+b}`);
+// }
 
-const measureTime = () => {
-    console.time("sum()");
-    sum(2,3);
-    console.timeEnd('sum()')
-    console.time("multiply()");
-    multiply(2,3);
-    console.timeEnd('multiply()')
-}
+// const multiply = (a,b) => {
+//     console.log(`Multiply of ${a} and ${b} is: ${a*b}`);
+// }
 
-measureTime()
+// const measureTime = () => {
+//     console.time("sum()");
+//     sum(2,3);
+//     console.timeEnd('sum()')
+//     console.time("multiply()");
+//     multiply(2,3);
+//     console.timeEnd('multiply()')
+// }
+
+// measureTime()
+
+// Progress bar -- CLI downloading bar
+const ProgressBar = require("progress");
+const bar = new ProgressBar("downloading [:bar] :rate/bps :precent :etas",{
+    total:20
+})
+
+const timer = setInterval(()=>{
+    bar.tick();
+    if(bar.complete){
+        clearInterval(timer);
+    }
+},100)
+
 
