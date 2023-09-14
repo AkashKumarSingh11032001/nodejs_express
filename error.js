35b42a10e214ff9e4dc4482f265b4a4cd6542b4f
Akash Singh <akashkumarsingh11032001@gmail.com>
2023-09-14 23:05:12 +0530

    using try n catch block

---
+++ error.js
@@ -0,0 +1,14 @@
+// method 1- using try-catch
+
+function doSomething() {
+//   console.log("I m doing something");
+  consol.log("I m doing something");
+}
+
+try {
+  doSomething();
+} catch (err) {
+  console.log("Error occured");
+  console.log(err);
+}
+
