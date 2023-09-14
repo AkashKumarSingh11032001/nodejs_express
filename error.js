b5536796a809dd146cd28d38a08258292b9bbb55
Akash Singh <akashkumarsingh11032001@gmail.com>
2023-09-14 23:14:21 +0530

    error handling with promise

--- error.js
+++ error.js
@@ -1,14 +1,38 @@
 // method 1- using try-catch
 
 function doSomething() {
-//   console.log("I m doing something");
-  consol.log("I m doing something");
+    console.log("I m doing something");
+//   consol.log("I m doing something");
 }
 
-try {
-  doSomething();
-} catch (err) {
-  console.log("Error occured");
-  console.log(err);
-}
+// try {
+//   doSomething();
+// } catch (err) {
+//   console.log("Error occured");
+//   console.log(err);
+// }
 
+// method 2- Uncaught Exception
+// process.on("uncaughtException", (err) => {
+//   console.log("There is an Uncaught Exception:", err);
+//   process.exit(1);
+// });
+
+// doSomething();
+
+// method 3- promise
+const myPromise = new Promise((resolve, reject) => {
+  if (true) {
+    resolve(doSomething());
+  } else {
+    reject(doSomething());
+  }
+});
+
+myPromise
+  .then((val) => {
+    console.log(val); // function not returing anything thats why undefined!
+  })
+  .catch((err) => {
+    console.log("Error Occcured", err);
+  });
