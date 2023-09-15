158f9eb7bdf1c4eee647cc5b59ef227f6a8b8ce8
Akash Singh <akashkumarsingh11032001@gmail.com>
2023-09-15 07:19:04 +0530

    nested chaining

--- promise.js
+++ promise.js
@@ -1,45 +1,63 @@
 // promise: reject and resolve
 // its ASYNC in nature.
 
-const promise = new Promise((resolve, reject) => {
-  console.log("ASYNC task execution");
-  if (false) {
-    resolve();
-  } else {
-    reject();
-  }
-});
+// const promise = new Promise((resolve, reject) => {
+//   console.log("ASYNC task execution");
+//   if (false) {
+//     resolve();
+//   } else {
+//     reject();
+//   }
+// });
 
-promise.then(
-  // if true
-  () => {
-    console.log("Passed!");
-  },
-  //   if false
-  () => {
-    console.log("Failed!");
-  }
-);
+// promise.then(
+//   // if true
+//   () => {
+//     console.log("Passed!");
+//   },
+//   //   if false
+//   () => {
+//     console.log("Failed!");
+//   }
+// );
 
-// parsing data in promise
-const myPromise = new Promise((resolve, reject) => {
-    console.log("ASYNC task execution");
-    if (true) {
-        const person = {name:"Akash"}
-      resolve(person);
-    } else {
-      reject();
-    }
-  });
+// // parsing data in promise
+// const myPromise = new Promise((resolve, reject) => {
+//     console.log("ASYNC task execution");
+//     if (true) {
+//         const person = {name:"Akash"}
+//       resolve(person);
+//     } else {
+//       reject();
+//     }
+//   });
   
-  myPromise.then(
-    // if true
-    (person) => {
-      console.log("Passed!");
-      console.log(person);
-    },
-    //   if false
-    () => {
-      console.log("Failed!");
-    }
-  );
+//   myPromise.then(
+//     // if true
+//     (person) => {
+//       console.log("Passed!");
+//       console.log(person);
+//     },
+//     //   if false
+//     () => {
+//       console.log("Failed!");
+//     }
+//   );
+
+//  nested chaining call
+const p = Promise.resolve("Task done!")
+p.then((val)=>{
+    console.log(val);
+    return "task done 2"
+}).then((val)=>{
+    console.log(val)
+    return "task done 3"
+}).then((val)=>{
+    console.log(val)
+    return "task done 4"
+
+}).then((val)=>{
+    console.log(val)
+    return "task done 5"
+
+}).catch((val)=>console.log(val))
