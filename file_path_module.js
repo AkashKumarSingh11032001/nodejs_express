f91a70f3d9b326ed5dada3b9c56fc21bf77e3d5c
Akash Singh <akashkumarsingh11032001@gmail.com>
2023-09-15 06:12:46 +0530

    writing file

--- file_path_module.js
+++ file_path_module.js
@@ -21,32 +21,42 @@ const fs = require("fs");
 // console.log(fs)
 
 // reading file -- Async way
-fs.readFile(filePath, "utf-8", (err, data) => {
-  if (err) throw new Error("Something went wrong");
-  console.log(data);
-});
+// fs.readFile(filePath, "utf-8", (err, data) => {
+//   if (err) throw new Error("Something went wrong");
+//   console.log(data);
+// });
 
 // reading file -- sync way
-try {
-  const data = fs.readFileSync(
-    path.join(__dirname, "file_test", "sample.txt"),
-    "utf-8"
-  );
-  console.log(data);
-} catch (err) {
-  console.log(err);
-}
+// try {
+//   const data = fs.readFileSync(
+//     path.join(__dirname, "file_test", "sample.txt"),
+//     "utf-8"
+//   );
+//   console.log(data);
+// } catch (err) {
+//   console.log(err);
+// }
 
 // reading file -- using PROMISE
-const myPromise = require("fs").promises;
+// const myPromise = require("fs").promises;
 
-const promiseFunc = async () => {
-  try {
-    const data = await myPromise.readFile(filePath, "utf-8");
-    console.log("FS PROMISE: ", data);
-  } catch (err) {
-    console.log(err);
-  }
-};
+// const promiseFunc = async () => {
+//   try {
+//     const data = await myPromise.readFile(filePath, "utf-8");
+//     console.log("FS PROMISE: ", data);
+//   } catch (err) {
+//     console.log(err);
+//   }
+// };
 
-promiseFunc();
+// promiseFunc();
+
+// -------------- WRITING INTO FILE ------------
+
+// writing file -- Async way
+const txtFile = path.join(__dirname, "file_test", "text.txt");
+const dataToWrite = "Hi Varsha!";
+fs.writeFile(txtFile, dataToWrite, (err) => {
+  if (err) throw new Error("Something went wrong");
+  console.log("Write opration completed Successfully!");
+});
