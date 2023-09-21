59a0c076f5880ebb1929ad96faa357d0d9d5a651
Akash Singh <akashkumarsingh11032001@gmail.com>
2023-09-21 10:50:48 +0530

    express callback

--- index.js
+++ index.js
@@ -2,10 +2,15 @@ const express = require("express");
 const app = express();
 const port = 3001;
 
-
 app.get("/", (req, res) => {
-    res.send("Hello World!");
-})
+  res.json({ message: "This is Home Page" });
+});
+app.get("/user", (req, res) => {
+  res.json({ message: "Get All Users" });
+});
+app.get("/user/:id", (req, res) => {
+  res.json({ message: `I am User With Id: ${req.params.id} ` });
+});
 
 app.listen(port, () => {
   console.log(`App listining at port: ${port}`);
